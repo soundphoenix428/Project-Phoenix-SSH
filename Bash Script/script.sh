74017ea448eb2563bf333e992814851c446bb755
Tanner <135170881+soundphoenix428@users.noreply.github.com>
2023-11-30 10:22:28 -0500

    Add script.sh

---
+++ Bash Script/script.sh
@@ -0,0 +1,12 @@
+#!/Bin/Bash
+set -e
+echo -e "\e[32m
+ ************************************************
+ *     Project Phoenix Stastistic Script        *
+ ************************************************\e[0m"
+# Welcome the user to the script 
+echo Welcome to the Project Phoenix Script 
+while true; do
+RESET="/e[0m"
+GREEN="e[32m"
+read -p "
