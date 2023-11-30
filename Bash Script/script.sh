18b4bb3dfe13bb45110182b91aebd4492ea7ddc4
Tanner <135170881+soundphoenix428@users.noreply.github.com>
2023-11-30 12:02:22 -0500

    Modify script.sh Add config.json

--- Bash Script/script.sh
+++ Bash Script/script.sh
@@ -3,7 +3,15 @@ set -e
 echo -e "\e[32m
  ************************************************
  *     Project Phoenix Stastistic Script        *
+ *    THIS SCRIPT CAN ONLY BE RUN IN UBUNTU     *
  ************************************************\e[0m"
+
+# Check if user is sudo
+if [[ $EUITD -ne 0 ]]; then
+    echo -e "\e{32m* Project Phoenix must be executed via sudo user. \e[0m" 1>&2
+    exit 1
+fi
+
 # Welcome the user to the script 
 echo Welcome to the Project Phoenix Script 
 while true; do
@@ -18,4 +26,40 @@ exit;;
 * ) echo -e "\3[32m* Invalid Response.\3[0m";;
 esac
 done
+echo -e "\e[32m* Installing dependencies..\3[0m"
+sudo apt update > /dev/null 2>&1
+#Install Curl and NPM
+if ! [ -x "$(command -v curl)" ]; then
+echo -e "\e[32m* Installing curl...\e[0m"
+sudo apt install -y curl > /dev/null 2>&1
+fi
 
+# Install NPM 
+if ! [ -x "$(command -v npm)" ]; then
+echo -e "\e[32m* Installing NPM...\e[0m"
+sudo apt install -y npm > /dev/null 2>&1
+fi 
+
+# Install Wget
+if ! [ -x "$(command -v wget)" ]; then
+echo -e "\e[32m* Installing wget...\e[0m"
+sudo apt install -y wget > /dev/null 2>&1
+fi
+
+# Install Java
+if ! [ -x "$(command -v javac)" ]; then
+echo -e "\e[32m* Installing java...\e[0m"
+sudo apt install -y default-jdk > /dev/null 2>&1
+fi
+
+# Install Pm2
+if ! [ -x "$(command -v pm2)" ]; then
+echo -e "\e[32m* Installing pm2...\e[0m"
+sudo npm install pm2 -g > /dev/null 2>&1
+fi
+
+
+# Start Project Phoenix
+sudo pm2 start ~/Project Phoenix/config.json --name "Project Phoenix"
+sudo pm2 startup
+sudo pm2 save
