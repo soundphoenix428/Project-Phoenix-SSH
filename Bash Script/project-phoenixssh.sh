38e2bf3ec84d6e85b080f61f15d333ecdc9d422c
Tanner <135170881+soundphoenix428@users.noreply.github.com>
2023-12-01 00:27:55 -0500

    Modify project-phoenixssh.sh

--- Bash Script/project-phoenixssh.sh
+++ Bash Script/project-phoenixssh.sh
@@ -16,8 +16,6 @@ fi
 while true; do
 RESET="\e[0m"
 GREEN="\e[32m"
-read -p "$(echo -e $GREEN"\n* Welcome to Project Phoenix Script"$RESET)" yn
-read -p "$(echo -e $GREEN"\n* Press Enter to continue..."$RESET)" yn
 read -p "$(echo -e $GREEN"\n* Do you want to proceed? (Y/N)"$RESET)" yn
 case $yn in
 [yY] ) echo -e "\e[32m* Confirmed. Continuing...\e[0m";
