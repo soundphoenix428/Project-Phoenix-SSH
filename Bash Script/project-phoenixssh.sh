001610d7b13ea06eaf25cde9c088a23afdd56623
Tanner <135170881+soundphoenix428@users.noreply.github.com>
2023-11-30 23:54:48 -0500

    Modify project-phoenixssh.sh

--- Bash Script/project-phoenixssh.sh
+++ Bash Script/project-phoenixssh.sh
@@ -12,50 +12,62 @@ if [[ $EUITD -ne 0 ]]; then
     exit 1
 fi
 
-# Welcome the user to the script 
-echo e[32m Welcome to the Project Phoenix Script /e[0m
+# Proceed?
 while true; do
-RESET="/e[0m"
-GREEN="e[32m"
-read -p "$(echo -e $GREEN"\n* Proceed? (Y/N)"$RESET)" yn
+RESET="\e[0m"
+GREEN="\e[32m"
+read -p "$(echo -e $GREEN"\n* Do you want to proceed? (Y/N)"$RESET)" yn
 case $yn in
-[yY] ) echo -e "\e[32m* Confirmed. Continuing..\e[0m";
+[yY] ) echo -e "\e[32m* Confirmed. Continuing...\e[0m";
 break;;
-[nN] ) echo -e "\e[32m* Confirmed. Exiting...\e[0m";
+[nN] ) echo -e "\e[32m* Confirmed. Exiting Installation...\e[0m";
 exit;;
 * ) echo -e "\3[32m* Invalid Response.\3[0m";;
 esac
 done
-echo -e "\e[32m* Installing dependencies..\3[0m"
+echo -e "\e[32m* Installing dependencies...\3[0m"
 sudo apt update > /dev/null 2>&1
 
-#Install Curl
+# Install Curl
 if ! [ -x "$(command -v curl)" ]; then
 echo -e "\e[32m* Installing curl...\e[0m"
 sudo apt install -y curl > /dev/null 2>&1
 fi
 
+# Install NodeJS
+if ! [ -x "$(command -v node)" ]; then
+echo -e "\e[32m* Installing NodeJS\e[0m"
+echo -e "\e[32m* Which NodeJS version would you like to install? []\e[0m"
+read NodeJSVer
+if [[ -n "NodeJSVer" ]]
+then
+in=$NodeJSVer
+fi
+echo -e "\e[32m* Installing NodeJS: $in \e[0m"
+curl -sL https://deb.nodesource.com/setup_$in.x | sudo -E bash - > /dev/null 2>&1 && sudo apt install -y nodejs > /dev/null 2>&1
+fi
+
 # Install NPM 
 if ! [ -x "$(command -v npm)" ]; then
-echo -e "\e[32m* Installing NPM...\e[0m"
+echo -e "\e[32m* Installing NPM.\e[0m"
 sudo apt install -y npm > /dev/null 2>&1
-fi 
+fi
 
 # Install Wget
 if ! [ -x "$(command -v wget)" ]; then
-echo -e "\e[32m* Installing wget...\e[0m"
+echo -e "\e[32m* Installing wget.\e[0m"
 sudo apt install -y wget > /dev/null 2>&1
 fi
 
 # Install Java
 if ! [ -x "$(command -v javac)" ]; then
-echo -e "\e[32m* Installing java...\e[0m"
+echo -e "\e[32m* Installing java.\e[0m"
 sudo apt install -y default-jdk > /dev/null 2>&1
 fi
 
 # Install Pm2
 if ! [ -x "$(command -v pm2)" ]; then
-echo -e "\e[32m* Installing pm2...\e[0m"
+echo -e "\e[32m* Installing pm2.\e[0m"
 sudo npm install pm2 -g > /dev/null 2>&1
 fi
 
